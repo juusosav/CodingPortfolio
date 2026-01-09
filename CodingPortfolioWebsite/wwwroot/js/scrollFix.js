8b5b6507bcdc977e35db4b3b28a345933f6b0505
Juuso <juuso.sav@gmail.com>
2026-01-09 09:40:57 +0200

    Fixed content stylings and site behaviour

---
+++ CodingPortfolioWebsite/wwwroot/js/scrollFix.js
@@ -0,0 +1,6 @@
+﻿window.forceScrollTopOnRefresh = () => {
+    if (window.location.hash) {
+        history.replaceState(null, "", window.location.pathname);
+        window.scrollTo(0, 0);
+    }
+};
